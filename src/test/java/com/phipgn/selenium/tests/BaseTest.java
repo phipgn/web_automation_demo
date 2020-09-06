0eb155fcaef87e49e321ad44ab2c8339805f047a
Phi <phipgn@gmail.com>
2020-09-06 13:45:17 +0700

    fix bugs

--- src/test/java/com/phipgn/selenium/tests/BaseTest.java
+++ src/test/java/com/phipgn/selenium/tests/BaseTest.java
@@ -1,28 +1,32 @@
 package com.phipgn.selenium.tests;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 import com.phipgn.selenium.utilities.Browser;
 
 public class BaseTest {
 	protected WebDriver driver;
 
-	@BeforeClass
+	@BeforeMethod
 	public void init() {
 		Browser.initialize();
 	}
 
-	@AfterSuite(alwaysRun = true)
+	@AfterMethod(alwaysRun = true)
 	public void tearDown() {
 		if (Browser.getDriver() != null)
 			Browser.getDriver().quit();
 	}
 
 	protected void waitFor(int time) {
-		Browser.getDriver().manage().timeouts().implicitlyWait(time, TimeUnit.SECONDS);
+		try {
+			Thread.sleep(time * 1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+//		Browser.getDriver().manage().timeouts().implicitlyWait(time, TimeUnit.SECONDS);
 	}
 }
